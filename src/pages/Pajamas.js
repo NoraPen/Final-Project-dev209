ceb4452bcfcff32e7c4e95379771c15f8ab2fe10
Nora Pence <nora.pence@bellevuecollege.edu>
2025-06-22 16:15:57 -0700

    Fix footer error

--- src/pages/Pajamas.js
+++ src/pages/Pajamas.js
@@ -75,17 +75,7 @@ function Pajamas() {
         </div>
       </main>
 
-      <footer className="footer-custom">
-        <div className="footer-content">
-          <p>📍 123 Sunshine St, Seattle, WA</p>
-          <p>📞 (123) 456-7890</p>
-          <p>📧 contact@minicloset.com</p>
-          <p>📸 Instagram: @minicloset_app</p>
-        </div>
-        <div className="footer-bottom">
-          <p>© 2025 Mini Closet. All rights reserved.</p>
-        </div>
-      </footer>
+      
       <Footer />
     </>
   );
